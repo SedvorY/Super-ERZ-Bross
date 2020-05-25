a3066f48c95ab41c6a836d7984a6429ff3eb3322
SedvorY <sedvory@gmail.com>
2020-05-26 01:28:42 +0200

    25_05_Room

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static LevelGenerator sharedInstance;
+
+    public List<LevelBlock> allTheLevelBlocks = new List<LevelBlock>();
+
+    public Transform levelStartPoint;
+
+    public List<LevelBlock> currentBlocks = new List<LevelBlock>();
+
+
+    private void Awake()
     {
-        
+        sharedInstance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void AddLevelBlock()
     {
-        
+        int randomIndex = Random.Range(0, allTheLevelBlocks.Count);
+
+        LevelBlock currentBlock = (LevelBlock)Instantiate(allTheLevelBlocks[randomIndex]);
+        currentBlock.transform.SetParent(this.transform, false);
+
+        Vector3 spawnPosition = Vector3.zero;
+
+        if (currentBlocks.Count == 0)
+        {
+            spawnPosition = levelStartPoint.position;
+        }
+        else
+        {
+            spawnPosition = currentBlocks[currentBlocks.Count - 1].exitPoint.position;
+        }
+
+        currentBlock.transform.position = spawnPosition;
+
+        currentBlocks.Add(currentBlock);
+
+    }
+
+    private void Start()
+    {
+        AddLevelBlock();
+    }
+
+    public void RemoveOldestLevelBlock()
+    {
+
+    }
+
+    public void RemoveAllTheBlocks()
+    {
+
+    }
+
+    public void GenerateInitialBlocks()
+    {
+
     }
 }
